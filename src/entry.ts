826f1c60ce59a62634ce387cd1e5ab841edc80e1
aa1213889 <869135421@qq.com>
2022-11-19 11:01:08 +0800

    test add

--- src/entry.ts
+++ src/entry.ts
@@ -1,10 +1,8 @@
 import type { App } from 'vue'
 import SButton from '@/button'
 
-// 导出单独组件
 export { SButton }
 
-// 编写一个插件，实现一个install方法
 export default {
   install(app: App) {
     app.component(SButton.name, SButton)
