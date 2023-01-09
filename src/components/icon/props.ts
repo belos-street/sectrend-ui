1cc3021cea75b90aef928f01a88bddfd5a3f5f98
zhi.jiang <zhi.jiang@sectrend.com.cn>
2023-01-09 16:45:23 +0800

    fix: 修改icon数据类型

--- src/components/icon/props.ts
+++ src/components/icon/props.ts
@@ -1,11 +1,11 @@
-import type { PropType, ExtractPropTypes, VNode } from 'vue'
+import type { PropType, ExtractPropTypes } from 'vue'
 
 export const iconProps = {
   rotate: Number,
   spin: Boolean,
   size: Number,
   color: String,
-  icon: Object as PropType<VNode>
+  icon: Object as PropType<any>
 }
 
 export type IconProps = ExtractPropTypes<typeof iconProps>
